6d7b8c69ed325c05bc0e87707f9ddc67770f3442
nandan858 <nandanpk90@gmail.com>
2022-10-15 07:29:44 +0530

    Completed Auth for Food And Restaurant Routes

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,7 +4,9 @@ var _express = _interopRequireDefault(require("express"));
 
 var _dotenv = _interopRequireDefault(require("dotenv"));
 
-var _connection = _interopRequireDefault(require("../database/connection"));
+var _connection = _interopRequireDefault(require("./database/connection"));
+
+var _index = _interopRequireDefault(require("./api/auth/index"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -16,7 +18,9 @@ zomato.get('/', (req, res) => {
   res.json({
     message: "Server is running"
   });
-});
+}); //auth/signup
+
+zomato.use("/auth", _index.default);
 const PORT = 4000;
 zomato.listen(PORT, () => {
   (0, _connection.default)().then(() => {
